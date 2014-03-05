28126ab5e17e8ed7bad3effffe1e1984e1293ecf
Will Ricketts <will@willricketts.com>
2014-03-05 21:40:36 +0000

    ugh

--- app.js
+++ app.js
@@ -29,9 +29,7 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', function(req, res){
-	res.render('index.ejs');
-});
+app.get('/', routes.index);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
