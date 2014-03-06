5c437c406ec25104b5c6d33fe3b8c3ee6ba67f7e
Will Ricketts <will@willricketts.com>
2014-03-06 14:11:51 +0000

    changed listening message

--- app.js
+++ app.js
@@ -32,5 +32,5 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+  console.log('Lighting controller listening on port ' + app.get('port'));
 });
